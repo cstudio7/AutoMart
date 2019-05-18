ee2c71db86757d5fb2c894057cb947b02f554342
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-05-18 13:01:27 +0100

    UI Update

---
+++ upload.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  if (window.File && window.FileList && window.FileReader) {
+    var filesInput = document.getElementById("uploadImage");
+    filesInput.addEventListener("change", function(event) {
+      var files = event.target.files;
+      var output = document.getElementById("result");
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        if (!file.type.match('image'))
+          continue;
+        var picReader = new FileReader();
+        picReader.addEventListener("load", function(event) {
+          var picFile = event.target;
+          var div = document.createElement("div");
+          div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+            "title='" + picFile.name + "'/>";
+          output.insertBefore(div, null);
+        });        
+        picReader.readAsDataURL(file);
+      }
+
+    });
+  }
+}
+
+
